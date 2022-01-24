6d970561c71e6c7a11341e3aef89a96f2729b6e7
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-24 14:24:24 +0200

    Render add product component

--- src/components/ProductsContainer.js
+++ src/components/ProductsContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductsList from "./ProductsList";
+import AddProduct from "./AddProduct";
 class ProductsContainer extends React.Component {
   state = {
     products: [
@@ -10,26 +11,38 @@ class ProductsContainer extends React.Component {
       },
       {
         elementName: "Baccalaureate Nurse & Patient-centered Care",
-        description: "Practice using caring, compassionate, culturally competent, and evidence-based practices in the roles of the baccalaureate nurse using the nursing process to provide patient/client-centered care in a variety of healthcare settings.",
+        elementDescription: "Practice using caring, compassionate, culturally competent, and evidence-based practices in the roles of the baccalaureate nurse using the nursing process to provide patient/client-centered care in a variety of healthcare settings.",
         elementID: "RN-BSN-PO1" 
       },
       {
         elementName: "Baccalaureate Nurse & Patient-centered Care",
-        description: "Practice using caring, compassionate, culturally competent, and evidence-based practices in the roles of the baccalaureate nurse using the nursing process to provide patient/client-centered care in a variety of healthcare settings.",
+        elementDescription: "Practice using caring, compassionate, culturally competent, and evidence-based practices in the roles of the baccalaureate nurse using the nursing process to provide patient/client-centered care in a variety of healthcare settings.",
         elementID: "RN-BSN-PO1" 
       },
       {
         elementName: "Baccalaureate Nurse & Patient-centered Care",
-        description: "Practice using caring, compassionate, culturally competent, and evidence-based practices in the roles of the baccalaureate nurse using the nursing process to provide patient/client-centered care in a variety of healthcare settings.",
+        elementDescription: "Practice using caring, compassionate, culturally competent, and evidence-based practices in the roles of the baccalaureate nurse using the nursing process to provide patient/client-centered care in a variety of healthcare settings.",
         elementID: "RN-BSN-PO1" 
       }
     ]
   }
 
+  // addProduct = (elementName, elementDescription, elementID) => {
+  //   const newProduct = {
+  //     elementName: elementName,
+  //     elementDescription: elementDescription,
+  //     elementID: elementID
+  //   }
+  //   this.setState({
+  //     products: [...this.state.products, newProduct]
+  //   })
+  // }
+
   render() {
     return (
       <div>
         <h1>Products Container</h1>
+        <AddProduct addProduct={this.addProduct} />
         <ProductsList products={this.state.products} />
       </div>
     );
