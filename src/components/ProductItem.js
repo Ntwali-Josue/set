d4bfb3c190e8304335afee413667d0e329a0edf7
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-24 15:49:21 +0200

    Render delete product funtion

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,12 +1,35 @@
 import React from "react";
 
 class ProductItem extends React.Component {
+
+  state = {
+    editing: false
+  }
+
+  handleEdit = () => {
+    this.setState({
+      editing: true
+    })
+  }
+
+  handleDoneEdit = (e) => {
+    if (e.key === 'Enter') {
+      this.setState({ editing: false })
+    }
+  }
+
   render() {
+
+
     return (
       <div className="product-item">
-        <h3>{this.props.product.elementName}</h3>
-        <p>{this.props.product.elementDescription}</p>
-        <span>{this.props.product.elementID}</span>
+        <div className="product-name">{this.props.product.elementName}
+        </div>
+        <div className="product-description">{this.props.product.elementDescription}
+        </div>
+        <div className="product-id">{this.props.product.elementID}
+        <button onClick={() => this.props.deleteProductProps(this.props.product.elementID)}>Delete</button>
+        </div>
       </div>
     );
   }
