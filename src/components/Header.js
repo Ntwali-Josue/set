707a282bcd9d237c79febb585673a0640dc1a872
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-24 13:54:08 +0200

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Header = () => {
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "1.5em",
+  }
+
+  return (
+    <header style={headerStyle}>
+    <h1>
+      Edit Set
+      <hr />
+    </h1>
+  </header>
+  )
+}
+
+export default Header;
