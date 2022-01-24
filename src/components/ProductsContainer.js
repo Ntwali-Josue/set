e7aa29a9b8d89ad38455662a458605a946b3683b
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-24 23:45:22 +0200

    Use 2 defaults outcomes

--- src/components/ProductsContainer.js
+++ src/components/ProductsContainer.js
@@ -6,7 +6,7 @@ class ProductsContainer extends React.Component {
   state = {
     products: [
       {
-        elementName: "Baccalaureate Nurse & Patient-centered Care",
+        elementName: "Baccalaureate Nurse & Patient-centered",
         elementDescription:
           "Practice using caring, compassionate, culturally competent, and evidence-based practices in the roles of the baccalaureate nurse using the nursing process to provide patient/client-centered care in a variety of healthcare settings.",
         elementID: "RN-BSN-PO1",
@@ -16,36 +16,6 @@ class ProductsContainer extends React.Component {
         elementDescription:
           "Use a broad base of techniques to communicate effectively with clients,families, healthcare teams, and communities.",
         elementID: "RN-BSN-PO2",
-      },
-      {
-        elementName: "Quality Healthcare in an Evolving System",
-        elementDescription:
-          "Use critical thinking and decision making, local, state, national and global policies, legislative concepts, and healthcare economics to effect quality healthcare in the evolving healthcare system.",
-        elementID: "RN-BSN-PO3",
-      },
-      {
-        elementName: "Leadership, Quality, Safety",
-        elementDescription:
-          "Integrate knowledge and skills in nursing leadership and management, quality improvement, and patient safety, as required, to provide healthcare.",
-        elementID: "RN-BSN-PO4",
-      },
-      {
-        elementName: "Promote Health & Prevent Disease",
-        elementDescription:
-          "Integrate knowledge and skills to promote health and prevent disease across the lifespan and the continuum of healthcare environments.",
-        elementID: "RN-BSN-PO5",
-      },
-      {
-        elementName: "Altruism, Integrity & Social Justice",
-        elementDescription:
-          "Practice professionalism, including the inherent values of altruism, autonomy, human dignity, integrity, and social justice.",
-        elementID: "RN-BSN-PO6",
-      },
-      {
-        elementName: "Understand Human Behavior",
-        elementDescription:
-          "Think critically at a conceptual level and by using mathematical analysis as well as the scientific method, write and speak effectively, use basic computer applications, and understand human behavior in the context of the greater society in a cultu...",
-        elementID: "RN-BSN-PO7",
       },
     ],
     add: false,
