fa74b90dfe5a0f81f65ef9a802a72046a2b88475
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-24 21:48:39 +0200

    Format the file

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -4,52 +4,74 @@ import add from "../images/add.svg";
 class AddProduct extends React.Component {
   state = {
     elementName: "",
-    elementDescription: "",	
-    elementID: ""
-  }
+    elementDescription: "",
+    elementID: "",
+  };
 
   handleNameChange = (e) => {
     this.setState({
       elementName: e.target.value,
-    })
-  }
+    });
+  };
 
   handleDescriptionChange = (e) => {
     this.setState({
       elementDescription: e.target.value,
-    })
-  }
+    });
+  };
 
   handleIDChange = (e) => {
     this.setState({
       elementID: e.target.value,
-    })
-  }
+    });
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addProduct(this.state.elementName, this.state.elementDescription, this.state.elementID);
+    this.props.addProduct(
+      this.state.elementName,
+      this.state.elementDescription,
+      this.state.elementID
+    );
     this.setState({
       elementName: "",
       elementDescription: "",
-      elementID: ""
-    })
-  }
+      elementID: "",
+    });
+  };
 
   render() {
     return (
       <div className="add-product">
-      <span className="selector"></span>
-      <form onSubmit={this.handleSubmit} className="form">
-        <input type="text" className="input-style" placeholder="Outcome name" value={this.state.elementName} onChange={this.handleNameChange} />
-        <input type="text" className="input-style desc-input" placeholder="Description" value={this.state.elementDescription} onChange={this.handleDescriptionChange} />
-        <input type="text" className="input-style id-input" placeholder="ID" value={this.state.elementID} onChange={this.handleIDChange} />
-        <button type="submit" className="submit-btn">
-          <img src={add} alt="submit" />
-        </button>
-      </form>
+        <span className="selector"></span>
+        <form onSubmit={this.handleSubmit} className="form">
+          <input
+            type="text"
+            className="input-style"
+            placeholder="Outcome name"
+            value={this.state.elementName}
+            onChange={this.handleNameChange}
+          />
+          <input
+            type="text"
+            className="input-style desc-input"
+            placeholder="Description"
+            value={this.state.elementDescription}
+            onChange={this.handleDescriptionChange}
+          />
+          <input
+            type="text"
+            className="input-style id-input"
+            placeholder="ID"
+            value={this.state.elementID}
+            onChange={this.handleIDChange}
+          />
+          <button type="submit" className="submit-btn">
+            <img src={add} alt="submit" />
+          </button>
+        </form>
       </div>
-    )
+    );
   }
 }
 
